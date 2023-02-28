28fe58259815dc9a4e66bb6f5641009993102201
ramizqazi <ramizqazi03@gmail.com>
2023-03-01 01:13:50 +0500

    learned dart json data mappaing

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,32 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widget/ItemWidget.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:flutter_application_1/modals/catelog.dart';
+import 'package:flutter_application_1/widget/drawer.dart';
+import 'package:flutter_application_1/widget/item_widget.dart';
 
-import '../widget/drawer.dart';
-import '../modals/catelog.dart';
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Codepur";
 
   @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 3));
+
+    // this rootBundle will get json and convert it to string
+    final jsonData = await rootBundle.loadString('assets/files/catelog.json');
+
+    // this will convert it back to json
+    final decodedJsonData = jsonDecode(jsonData);
+    final productsData = decodedJsonData["products"];
+
+    CatelogModal.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatelogModal.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(18),
-        itemCount: dummyList.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(
-            item: dummyList[index],
-          );
-        },
-      ),
+      body: CatelogModal.items != null
+          ? ListView.builder(
+              padding: const EdgeInsets.all(18),
+              itemCount: CatelogModal.items!.length,
+              itemBuilder: (context, index) {
+                return ItemWidget(
+                  item: CatelogModal.items![index],
+                );
+              },
+            )
+          : Center(
+              child: CircularProgressIndicator(color: Colors.deepPurple),
+            ),
       drawer: MyDrawer(),
     );
   }
--- lib/widget/item_widget.dart
+++ lib/widget/item_widget.dart
@@ -13,9 +13,9 @@ class ItemWidget extends StatelessWidget {
         onTap: () {
           print("${item.name} pressed");
         },
-        leading: Image.network(item.image),
-        title: Text(item.name),
-        subtitle: Text(item.desc),
+        leading: Image.network(item.image ?? ''),
+        title: Text(item.name ?? ''),
+        subtitle: Text(item.desc ?? ''),
         trailing: Text(
           '\$${item.price.toString()}',
           textScaleFactor: 1.2,
